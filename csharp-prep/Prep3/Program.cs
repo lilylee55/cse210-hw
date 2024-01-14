fd092110b6fa445c6438378987ece83dfa1f3314
lilylee55 <lilylee55@hotmail.com>
2024-01-13 20:43:13 -0700

    prep 3 core 2

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
@@ -7,20 +8,26 @@ class Program
         Console.Write("What is the magic number?: ");
         int magicNumber = int.Parse(Console.ReadLine());
 
-        Console.Write("What is your guess?: ");
-        int guess = int.Parse(Console.ReadLine());
+        //Add a loop that keeps looping as long as the guess does not match the magic number.
+        int guess = -1;
+        while (magicNumber != guess)
+        {
+            Console.Write("What is your guess?: ");
+            guess = int.Parse(Console.ReadLine());
 
-        if (magicNumber > guess)
-        {
-            Console.WriteLine("Higher");
-        }
-        else if (magicNumber < guess)
-        {
-            Console.WriteLine("Lower");
-        }
-        else
-        {
-            Console.WriteLine("You guessed it!");
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
         }
     }
+
 }
