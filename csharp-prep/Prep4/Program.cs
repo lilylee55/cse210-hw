1caaf210ad789a2e9a3f861a027292b1c0ae9fad
lilylee55 <lilylee55@hotmail.com>
2024-01-13 23:54:06 -0700

    Prep 4 Core Requirements Finished

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        //Ask the user for a series of numbers, and append each one to a list. Stop when they enter 0.
+        List<int> numbers = new List<int>();
+        int number = -1;
+        while (number != 0)
+        {
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+                    //Compute the sum, or total, of the numbers in the list.
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        //Compute the average of the numbers in the list.
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        //find the largest number in the list.
+        int max = numbers[0];
+        foreach (int num in numbers)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+        Console.WriteLine($"The largest number is: {max}");
+        
     }
 }
