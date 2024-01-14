15f20f1de715cd9622f591195bee2f22c5d50177
lilylee55 <lilylee55@hotmail.com>
2024-01-13 19:15:13 -0700

    Prep 02 Finished

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Grade Percentage: ");
+        string grade = Console.ReadLine();
+        int gradePercentage = int.Parse(grade);
+        
+        string letter = "";
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        
+        string sign = "";
+        if ((gradePercentage % 10 >= 7 && gradePercentage >= 90) || (gradePercentage < 60))
+        {
+            sign = "";
+        }
+        else if (gradePercentage % 10 >= 7)
+        {
+            sign = "+";
+        }
+        else if (gradePercentage % 10 < 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        // show letter and sign
+        Console.WriteLine($"Your grade is {letter}{sign}.");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you failed. You can do better next time.");
+        }
     }
 }
