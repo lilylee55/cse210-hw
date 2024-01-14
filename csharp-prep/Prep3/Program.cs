f5604f7e94c3b09d8c3cdbe727823e3d7757fa7e
lilylee55 <lilylee55@hotmail.com>
2024-01-13 22:41:42 -0700

    prep 3 stretch challenge finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,29 +5,37 @@ class Program
 {
     static void Main(string[] args)
     {
+        string playAgain = "yes";
         Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 101);
 
-        //Add a loop that keeps looping as long as the guess does not match the magic number.
-        int guess = -1;
-        while (magicNumber != guess)
+        while (playAgain == "yes")
         {
-            Console.Write("What is your guess?: ");
-            guess = int.Parse(Console.ReadLine());
+            int magicNumber = randomGenerator.Next(1, 101);
+            int guess = -1;
+            int guessCount = 0;
+            while (magicNumber != guess)
+            {
+                Console.Write("What is your guess?: ");
+                guess = int.Parse(Console.ReadLine());
 
-            if (magicNumber > guess)
-            {
-                Console.WriteLine("Higher");
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+                guessCount++;
             }
-            else if (magicNumber < guess)
-            {
-                Console.WriteLine("Lower");
-            }
-            else
-            {
-                Console.WriteLine("You guessed it!");
-            }
+            Console.WriteLine($"You guessed it in {guessCount} guesses.");
+            Console.Write("Would you like to play again? (yes/no): ");
+            playAgain = Console.ReadLine();    
         }
-    }
 
+    }
 }
